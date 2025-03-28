365dc45e1fe0faff6a2c9f844fde0e61e759a4d4
Theophle Niyigaba <niyigabatheo10@gmail.com>
2025-03-28 21:12:04 +0200

    DEBUG  Falsse

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-vld^9b*t5%=aw18a=e93ml=)ipu=#sdpo$&aba$k61r8=%$@#e"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['crud-django-lfga.onrender.com', 'localhost', '127.0.0.1']
 
